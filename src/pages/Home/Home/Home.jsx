bc2c08ce33f6093caa0c98d6305b91f7b7ee9eb3
RubelHasan <rubelhasan306@gmail.com>
2023-10-31 04:16:56 +0000

    Navbar, Footer, Home Banner slider, Home category swiper slider done

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import CategorySlider from "../CategorySlider/CategorySlider";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <CategorySlider></CategorySlider>
+    </div>
+  );
+};
+
+export default Home;
